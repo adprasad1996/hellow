dfdf1259854386395fc57a3ca0b85ff88d0f7bd1
adprasad1996 <alladurgaprasad1996@gmail.com>
2024-03-06 16:30:48 +0530

    added one

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>31</li>
+        <li>4</li>
+        <li>6</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
